5257e257c3005bc7a67c49200394b5c192829884
shuai <shuai.wang.kaos@gmail.com>
2018-04-04 11:06:20 +0200

    add index for lesson 13

---
+++ app.js
@@ -0,0 +1,37 @@
+const person = {
+  firstName: 'shuai',
+  lastName: 'wang',
+  age: 30,
+  hobbies: ['music', 'sport'],
+  address: {
+    city: 'Milan',
+    state: 'Lombardia'
+  },
+  getBirthYear: function(){
+    return 2018 - this.age;
+  }
+
+};
+
+let val;
+val = person.age;
+
+val = person.getBirthYear();
+
+val = person.address.city;
+
+val = person.address.state;
+
+console.log(val);
+
+const people = [
+  {name: 'Mike', age: 30},
+  {name: 'Yan', age: 20},
+  {name: 'Shuai', age: 29}
+
+];
+
+for(let i = 0; i < people.length; i++){
+  console.log(people[i].name);
+  console.log(people[i].age);
+}
