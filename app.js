7d5e3da5622e3b8f786890b63d73483f73971545
shuai <shuai.wang.kaos@gmail.com>
2018-04-05 12:14:13 +0200

    add lesson 16 switch lesson script

--- app.js
+++ app.js
@@ -150,3 +150,46 @@ const age ='16';
 //  console.log('correct');
 //  else
 //  console.log('incorrect');
+
+// lesson 16 Switches
+
+const color = 'yellow';
+
+
+switch(color){
+  case 'red':
+    console.log('color is red');
+    break;
+  case 'blue':
+    console.log('color is blue');
+    break;
+  default:
+    console.log('color is not red or blue');
+    break;
+}
+
+switch(new Date().getDay()){
+  case 0:
+    day = 'Sunday';
+    break;
+  case 1:
+    day = 'Monday';
+    break;
+  case 2:
+    day = 'Tuesday';
+    break;
+  case 3:
+    day = 'Wednesday';
+    break;
+  case 4:
+    day = 'Thursday';
+    break;
+  case 5:
+    day = 'Friday';
+    break;
+  case 6:
+    day = 'Saturday';
+    break;
+}
+
+console.log(`Today is ${day}`)
