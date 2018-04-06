db6a4c40872762a2c05275ab10229dfb79e1ca87
shuai <shuai.wang.kaos@gmail.com>
2018-04-06 12:06:37 +0200

    add lesson 23 DOM querySelector for single element scripts

--- app.js
+++ app.js
@@ -433,3 +433,43 @@ linksArr.forEach(function(link){
 });
 
 console.log(val);
+
+//DOM Selectors For Single Elements lesson 23
+
+//document.getElementById()
+
+// console.log(document.getElementById('task-title'));
+
+// //Get things from the element
+
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
+
+// const taskTitle = document.getElementById('task-title');
+
+// //change styling
+// taskTitle.style.background = '#333';
+// taskTitle.style.color = '#fff';
+// taskTitle.style.padding = '5px';
+// // document.getElementById('task-title').style.display = 'none';
+
+// //change the content
+// taskTitle.textContent = 'Task List';
+// taskTitle.innerText = 'My Tasks';  
+// taskTitle.innerHTML = '<span style = "color: red"> Task List</span>';  
+
+
+//document.querySelector()
+
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('h5'));
+
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(3)').style.color = 'green';
+document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
