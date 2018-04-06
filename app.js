a924e44a0eee1d39e2ec0cd95405e7f5e67b815f
shuai <shuai.wang.kaos@gmail.com>
2018-04-06 10:34:11 +0200

    add new html index file and scripts fpr lesson 22 examing the document object

--- app.js
+++ app.js
@@ -371,5 +371,65 @@ val = window.navigator.platform;
 val = window.navigator.vendor;
 val = window.navigator.language;
 
+console.log(val);
+
+//Global Scope lesson 20
+
+// lesson 21 DOM
+// what is DOM
+// DOCUMENT OBJECT MODEL
+// tree of nodes /elements created by the browser
+// JavaScript can be used to read/write/manipulate to the DOM
+// Object Oriented Representation
+
+//EXAMING THE DOCUMENT OBJECT lesson 22
+
+let val;
+
+val = document;
+val = document.all;
+val = document.all[2];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+val = document.forms[0].action;
+
+val =  document.links;
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className;
+val = document.links[0].classList;
+
+val = document.images;
+
+val = document.scripts;
+val = document.scripts[2].getAttribute('src');
+
+//array from scripts
+let scripts = document.scripts;
+let scriptsArr = Array.from(scripts);
+
+scriptsArr.forEach(function(script){
+  console.log(script.getAttribute('src'));
+});
+
+// array from links
+
+let links = document.links;
+let linksArr = Array.from(links);
+
+linksArr.forEach(function(link){
+  console.log(link.getAttribute('class'));
+});
 
 console.log(val);
