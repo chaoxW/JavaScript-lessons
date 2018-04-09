b0b9dea9fb11278d94a526416f02ad53764e35e6
shuai <shuai.wang.kaos@gmail.com>
2018-04-09 11:19:33 +0200

    add lesson 26 scripts create new element

--- app.js
+++ app.js
@@ -585,3 +585,34 @@ val = listItem.previousSibling;
 val = listItem.previousElementSibling;
 
 console.log(val);
+
+// Create elements lesson 26
+
+const li = document.createElement('li');
+
+// Add class
+li.className = 'collection-item';
+
+//Add id
+li.id = 'new-item';
+
+// Add attribute
+li.setAttribute('title', 'New Item');
+
+// Create text node and append
+li.appendChild(document.createTextNode('Hello World'));
+
+// Create new link element
+const link = document.createElement('a');
+// Add class
+link.className = 'delete-item secondary-content';
+// Add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>'
+
+// Append link into li
+li.appendChild(link);
+
+// Apppend li as child to ul
+document.querySelector('ul.collection').appendChild(li);
+
+console.log(li);
