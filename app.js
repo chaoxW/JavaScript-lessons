ae356b3ee1b8254c596b6d003bd0db6ecc9ca18a
shuai <shuai.wang.kaos@gmail.com>
2018-04-12 11:10:21 +0200

    add lesson 28 scripts addEventListener

--- app.js
+++ app.js
@@ -671,3 +671,45 @@ val = link;
 
 
 console.log(val);
+
+// Event listenser & Event Object lesson 28
+
+// document.querySelector('.clear-tasks').addEventListener('click',
+// function(e){
+//   console.log('Hello World');
+//   // e.preventDefault();
+// });
+
+document.querySelector('.clear-tasks').addEventListener('click',onclick);
+
+function onclick(e){
+  // console.log('clicked');
+
+  let val;
+
+  val = e;
+
+  // Event target element
+  val = e.target;
+  val = e.target.id;
+  val = e.target.className;
+  val = e.target.classList;
+
+  e.target.innerText = 'Hello';
+
+  // Event type
+  val = e.type;
+
+  // Time stamp
+  val = e.timeStamp;
+
+  // Coords event relative to the window
+  val = e.clientY;
+  val = e.clientX;
+  
+  // Coords event relative to the element
+  val = e.offsetY;
+  val = e.offsetX;
+
+  console.log(val);
+}
